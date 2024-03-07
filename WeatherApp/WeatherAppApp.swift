12925677d66ec47c2014230ed2b2c401407e572c
Md Forhadul Islam <forhadreza1596@gmail.com>
2024-03-07 12:24:15 +0000

    update

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Reza on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
